5cf740d07e32abe059cc0e31c947bbc13304a744
JCat3 <jcatalano333@yahoo.com>
2022-01-03 12:49:25 -0600

    added app.js file with UFO table info. Looped throgh table information and added new row for each value

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,28 @@
+//console.log("app.js loaded");
+//console.log(data);
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// select the table
+var table = d3.select("ufo_table");
+
+table.attr("class", "table table-striped");
+
+var tbody = d3.select("tbody");
+
+// loop through UFO Data
+data.forEach(function(data) {
+    
+    //verify we're working with correct data
+    console.log(data);
+    // add new row for each UFO
+    var row = tbody.append("tr");
+
+    Object.entries(data).forEach(function([key, value]){
+        console.log(`key = ${key}, value = ${value}`);
+    });
+
+});
