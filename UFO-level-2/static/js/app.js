86a746a03cab0f4dd3151123658bae475bbeb824
JCat3 <jcatalano333@yahoo.com>
2022-01-03 18:31:55 -0600

    created function for ufo table and added button

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -13,16 +13,51 @@ table.attr("class", "table table-striped");
 
 var tbody = d3.select("tbody");
 
-// loop through UFO Data
-data.forEach(function(data) {
+// create UFO table function
+
+function ufo_table (data) {
+    // select the table
+    var table = d3.select("ufo_table");
+
+    table.attr("class", "table table-striped");
+
+    var tbody = d3.select("tbody");
+    tbody.html("");
+
+    // loop through UFO Data
+    data.forEach(function(data) {
     
     //verify we're working with correct data
-    console.log(data);
+        //console.log(data);
     // add new row for each UFO
-    var row = tbody.append("tr");
+        var row = tbody.append("tr");
 
-    Object.entries(data).forEach(function([key, value]){
-        console.log(`key = ${key}, value = ${value}`);
+        Object.entries(data).forEach(function([key, value]){
+            //console.log(`key = ${key}, value = ${value}`);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+
     });
+}
 
-});
+ufo_table(tableData);
+
+// Getting a reference to the button on the page with the id property set to `click-me`
+var button = d3.selectAll("#filter-btn");
+
+// Getting a reference to the input element on the page with the id property set to 'input-field'
+var inputField = d3.selectAll("#datetime");
+
+// This function is triggered when the button is clicked
+    function handleClick() {
+        console.log("A button was clicked!");
+
+   // We can use d3 to see the object that dispatched the event
+   console.log(d3.event.target);
+ }
+
+// // We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
+// function to filter data - need table as a function - clear table of all other info when filtering - create another table
+
